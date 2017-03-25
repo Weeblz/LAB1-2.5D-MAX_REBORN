1cc2409c2673d53b8d14931696cef0b1c19f665d
weeblz <wiblz123@gmail.com>
2017-03-25 22:27:16 +0200

    initial preparations
    Linked glm, sdl and glew to project. The reborn has begun.

---
+++ Lab1/Display.h
@@ -0,0 +1,6 @@
+#pragma once
+class Display {
+	Display();
+	virtual ~Display();
+	bool isOpened();
+};
---
+++ Lab1/Source.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <GL/glew.h>
+
+int main() {
+	std::cout << "THE REBORN HAS BEGUN!";
+	return 0;
+}
